3dfb54b18926ef439c57875cde7e6703d71c709f
Daniel Ancuta <Daniel.Ancuta@komputronik.pl>
2011-05-11 08:21:06 +0200

    Prepared admin panel

---
+++ modules/sfGuardObjectAuthorInfoBackend/actions/actions.class.php
@@ -0,0 +1,15 @@
+<?php
+require_once dirname(__FILE__).'/../lib/sfGuardObjectAuthorInfoBackendGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/sfGuardObjectAuthorInfoBackendGeneratorHelper.class.php';
+
+/**
+ * sfGuardObjectAuthorInfoBackend actions.
+ *
+ * @package    memgenerator.pl
+ * @subpackage sfGuardObjectAuthorInfoBackend
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardObjectAuthorInfoBackendActions extends autoSfGuardObjectAuthorInfoBackendActions
+{
+}
---
+++ modules/sfGuardObjectAuthorInfoBackend/lib/sfGuardObjectAuthorInfoBackendGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * sfGuardObjectAuthorInfoBackend module helper.
+ *
+ * @package    memgenerator.pl
+ * @subpackage sfGuardObjectAuthorInfoBackend
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfGuardObjectAuthorInfoBackendGeneratorHelper extends BaseSfGuardObjectAuthorInfoBackendGeneratorHelper
+{
+}
